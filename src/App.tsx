a7913561c3227e309244f154990ce3cb9ec6734b
aparna1606 <vaaparna2001@gmail.com>
2024-09-14 16:40:27 -0400

    getting to know branches

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and build the best UI
         </a>
       </header>
     </div>
