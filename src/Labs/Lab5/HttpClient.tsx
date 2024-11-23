6b36711b6bfe8bd92ad5da72375c85a10a3483b4
aparna1606 <vaaparna2001@gmail.com>
2024-11-22 21:21:32 -0500

    Netlify errros

--- src/Labs/Lab5/HttpClient.tsx
+++ src/Labs/Lab5/HttpClient.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import * as client from "./Client";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function HttpClient() {
     const [welcomeOnClick, setWelcomeOnClick] = useState("");
-    const [welcomeOnLoad, setWelcomeOnLoad] = useState("");
+    const [welcomeOnLoad] = useState("");
 
     const fetchWelcomeOnClick = async () => {
         const message = await client.fetchWelcomeMessage();
